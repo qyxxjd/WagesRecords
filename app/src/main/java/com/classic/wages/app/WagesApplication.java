2e91b2e73ea92534e82cd57f46723f7230b8b360
qyxxjd <pgliubin@gmail.com>
2016-11-18 16:34:48 +0800

    Commit version 5.0.2

--- app/src/main/java/com/classic/wages/app/WagesApplication.java
+++ app/src/main/java/com/classic/wages/app/WagesApplication.java
@@ -7,7 +7,6 @@ import com.classic.wages.di.components.AppComponent;
 import com.classic.wages.di.components.DaggerAppComponent;
 import com.classic.wages.di.modules.AppModule;
 import com.classic.wages.di.modules.DbModule;
-import com.squareup.leakcanary.LeakCanary;
 
 public class WagesApplication extends Application {
     private static SharedPreferencesUtil sPreferencesUtil;
@@ -17,10 +16,10 @@ public class WagesApplication extends Application {
     @Override public void onCreate() {
         super.onCreate();
         sPreferencesUtil = new SharedPreferencesUtil(this, Consts.SP_NAME);
-        if (LeakCanary.isInAnalyzerProcess(this)) {
-            return;
-        }
-        LeakCanary.install(this);
+        //if (LeakCanary.isInAnalyzerProcess(this)) {
+        //    return;
+        //}
+        //LeakCanary.install(this);
         mAppComponent = DaggerAppComponent.builder()
                                           .appModule(new AppModule(this))
                                           .dbModule(new DbModule())
